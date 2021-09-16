0cc7d5fe770a54f6f84261840db6a6b2029dcb28
rajashekhar <rajashekharpasikanti@gmail.com>
2021-09-16 15:00:11 +0530

    validLastName

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,4 +1,5 @@
 package com.bl;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,19 +8,34 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		UserRegistration registration = new UserRegistration();
-		System.out.println("name");
-		String name = scan.next();
-		registration.validFirstName(name);
+		System.out.println("Enter First name");
+		String fname = scan.next();
+		registration.validFirstName(fname);
+		System.out.println("Enter last name");
+		String lname = scan.next();
+		registration.validLastName(lname);
+		
+		
 	}
 
 	private void validFirstName(String firstName) {
-		String regex = "[A-Z][A-Za-z]{6,8}";
+		String regex = "[A-Z][A-Za-z]{2,9}";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(firstName);
 		if (matcher.matches()) {
 			System.out.println("User first name is valid :) ");
 		} else {
 			System.out.println("User first name is Invalid :( ");
+		}
+	}
+	private void validLastName(String firstName) {
+		String regex = "[A-Za-z]{2,15}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		if (matcher.matches()) {
+			System.out.println("User Last name is valid");
+		} else {
+			System.out.println("User Last name is Invalid");
 		}
 	}
 }
